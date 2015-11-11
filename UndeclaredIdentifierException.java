5cfa4690e944fb6baf3c531522307f483edc6a5e
unknown <gs.mendes@campus.fct.unl.pt>
2015-11-11 10:15:21 +0000

    misc

--- UndeclaredIdentifierException.java
+++ UndeclaredIdentifierException.java
@@ -5,6 +5,7 @@ public class UndeclaredIdentifierException extends Exception{
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	@SuppressWarnings("unused")
 	private String id;
 	
 	public UndeclaredIdentifierException(String id){
